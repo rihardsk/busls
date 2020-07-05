df13600d8464172d0d32e61b8b070a449489d40c
Rihards Krišlauks <rihards.krislauks@gmail.com>
2020-07-05 22:02:48 +0300

    Scrape all relevant pages
    The parser had to be adjusted because of tables for routes back to
    Riga.

--- Main.hs
+++ Main.hs
@@ -8,6 +8,8 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Prelude hiding (readFile, unwords)
 import Control.Applicative (many)
+import Text.Printf (printf)
+import Control.Monad
 
 data BusInfo = BusInfo
   { num   :: Text
@@ -21,35 +23,50 @@ data BusInfo = BusInfo
 (/.) :: Selector -> Selector -> Selector
 s1 /. s2 = s1 // s2 `atDepth` 1
 
-table :: IO (Maybe [BusInfo])
-table = scrapeSource nums
+table :: Int -> Int -> IO (Maybe [BusInfo])
+table routeId stationId = scrapeSource nums
   where
     scrapeSource :: Scraper Text a -> IO (Maybe a)
-    -- scrapeSource = scrapeURL "http://galssbuss.lv/pages/info_detail.php?l=lv&id_section=3&id_route=2&id_station=4"
-    scrapeSource scraper = do
-      page <- readFile "page.html"
-      return $ scrapeStringLike page scraper
+    scrapeSource = scrapeURL $ printf "http://galssbuss.lv/pages/info_detail.php?l=lv&id_section=3&id_route=%i&id_station=%i" routeId stationId
+    -- scrapeSource scraper = do
+    --   page <- readFile "page.html"
+    --   return $ scrapeStringLike page scraper
 
+    clean :: Text -> Text
+    clean = T.unwords . T.words
 
     nums :: Scraper Text [BusInfo]
     nums = chroots  "tbody" $ inSerial $ do
       (num, route) <- seekNext $ chroot  "tr" $ inSerial $ do
-        num   <- seekNext . text $ "td" // "p"
-        route <- seekNext . text $ "td" // "p"
+        num   <- seekNext . text $ "td"
+        route <- seekNext . text $ "td"
         return (num, route)
       (from, days) <- seekNext $ chroot  "tr" $ inSerial $ do
-        from <- seekNext . text $ "td" // "p"
+        from <- seekNext . text $ "td"
         seekNext $ matches "td"
-        days <- seekNext . text $ "td" // "p"
+        days <- seekNext . text $ "td"
         return (from, days)
       timess <- untilNext (matches $ "tr" // "td" // "p" ) $ many $ do
         times <- seekNext . texts $ "tr" /. "td"
         return times
 
-      let times = filter (not . T.null) . concat $ timess
-          days' = T.unwords . T.words $ days
-      return $ BusInfo num route from days' times
+      let num'   = clean num
+          route' = clean route
+          from'  = clean from
+          days'  = clean days
+          times  = filter (not . T.null) . concat $ timess
+      return $ BusInfo num' route' from' days' times
 
+ids =
+  [ (2, 4)
+  , (2, 5)
+  , (3, 8)
+  , (3, 9)
+  , (4, 13)
+  , (4, 14)
+  ]
 
 main :: IO ()
-main = table >>= print
+main = mapM_ (print <=< uncurry table) ids
+--   (routeId, stationId) <- ids
+--   table routeId stationId >>= print
